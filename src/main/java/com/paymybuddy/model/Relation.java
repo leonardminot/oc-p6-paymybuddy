f11db59322c74f99a29bd12834f90f2a2acd2179
leonard.minot <leonard.minot@gadz.org>
2023-11-21 16:03:56 +0100

    Add models for relation with a specific RelationId class.

---
+++ src/main/java/com/paymybuddy/model/Relation.java
@@ -0,0 +1,38 @@
+package com.paymybuddy.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity(name = "Relation")
+@Table(name = "relation")
+public class Relation {
+    @EmbeddedId
+    private RelationId relationId;
+
+    @ManyToOne
+    @MapsId("userId1")
+    @JoinColumn(name = "user_id_1")
+    private UserAccount user1;
+
+    @ManyToOne
+    @MapsId("userId2")
+    @JoinColumn(name = "user_id_2")
+    private UserAccount user2;
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    public Relation(UserAccount user1, UserAccount user2, Timestamp createdAt) {
+        this.user1 = user1;
+        this.user2 = user2;
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/com/paymybuddy/model/RelationId.java
@@ -0,0 +1,21 @@
+package com.paymybuddy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class RelationId implements Serializable {
+    @Column(name = "user_id_1")
+    private UUID userId1;
+    @Column(name = "user_id_2")
+    private UUID userId2;
+}
