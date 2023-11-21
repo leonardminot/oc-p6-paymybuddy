2fd748c138696c045a929203391cb89840573648
leonard.minot <leonard.minot@gadz.org>
2023-11-21 14:43:15 +0100

    Initial configuration. Authentication removed, configuration to connect to database done.

---
+++ src/main/java/com/paymybuddy/PayMyBuddyAppApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBuddyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayMyBuddyAppApplication.class, args);
+    }
+
+}
